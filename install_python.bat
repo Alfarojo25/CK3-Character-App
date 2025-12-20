d160d7f98e193d818e3dbed72bb5b65aea49b52f
Alfarojo25 <estebandubles@gmail.com>
2025-12-19 21:04:06 -0300

    Update Python version to 3.14 in install script

--- install_python.bat
+++ install_python.bat
@@ -34,7 +34,7 @@ winget --version >nul 2>&1
 if %errorlevel% equ 0 (
     echo Using Windows Package Manager to install Python...
     echo.
-    winget install -e --id Python.Python.3.11
+    winget install -e --id Python.Python.3.14
     if %errorlevel% equ 0 (
         echo.
         echo [OK] Python installed successfully!
@@ -67,7 +67,7 @@ echo.
 echo Please install Python manually:
 echo.
 echo 1. Go to https://www.python.org/downloads/windows/
-echo 2. Download Python 3.11 or newer
+echo 2. Download Python 3.14 or newer
 echo 3. Run the installer
 echo 4. IMPORTANT: Check "Add Python to PATH" during installation
 echo 5. Complete the installation
